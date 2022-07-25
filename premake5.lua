28e412823df5d6e336e2770f568bdd0a707388cf
Kamil Kerimov <koma_2002@bk.ru>
2022-07-26 01:01:25 +0500

    Premake and gitignore added

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+workspace "monk"
+   configurations { "Debug", "Release" }
+   architecture "x86_64"
+
+outputdir = "%{cfg.architecture}-%{cfg.buildcfg}"
+
+project "monk"
+   location "monk"
+   kind "ConsoleApp"
+   language "C++"
+   cppdialect "C++17"
+
+   targetdir ("bin/" .. outputdir)
+   objdir ("bin-int/" .. outputdir)
+
+   characterset ("ASCII")
+
+   files {
+      "monk/src/**"
+   }
+
+   includedirs {
+      "./monk/src"
+   }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      optimize "On"
+      symbols "Off"
